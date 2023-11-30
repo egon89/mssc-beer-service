0b80af128096c4cf309b421dcadf279717aed6f0
everton_goncalves <everton.goncalves.89@gmail.com>
2023-11-30 18:28:11 -0300

    feat: beer loader

--- src/main/java/com/egon/msscbeerservice/beer/repositories/BeerRepository.java
+++ src/main/java/com/egon/msscbeerservice/beer/repositories/BeerRepository.java
@@ -1,9 +1,10 @@
 package com.egon.msscbeerservice.beer.repositories;
 
 import com.egon.msscbeerservice.beer.entities.Beer;
+import org.springframework.data.repository.CrudRepository;
 import org.springframework.data.repository.PagingAndSortingRepository;
 
 import java.util.UUID;
 
-public interface BeerRepository extends PagingAndSortingRepository<Beer, UUID> {
+public interface BeerRepository extends CrudRepository<Beer, UUID>, PagingAndSortingRepository<Beer, UUID> {
 }
---
+++ src/main/java/com/egon/msscbeerservice/bootstrap/BeerLoader.java
@@ -0,0 +1,46 @@
+package com.egon.msscbeerservice.bootstrap;
+
+import com.egon.msscbeerservice.beer.entities.Beer;
+import com.egon.msscbeerservice.beer.repositories.BeerRepository;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.stereotype.Component;
+
+import java.math.BigDecimal;
+
+@Slf4j
+@Component
+public class BeerLoader implements CommandLineRunner {
+
+    private final BeerRepository beerRepository;
+
+    public BeerLoader(BeerRepository beerRepository) {
+        this.beerRepository = beerRepository;
+    }
+
+    @Override
+    public void run(String... args) throws Exception {
+        log.info("Starting beer loader...");
+        if (beerRepository.count() != 0) return;
+
+        beerRepository.save(
+                Beer.builder()
+                        .name("Mango Bobs")
+                        .style("IPA")
+                        .quantityToBrew(200)
+                        .minOnHand(12)
+                        .upc(337010000001L)
+                        .price(new BigDecimal("12.95"))
+                        .build());
+        beerRepository.save(
+                Beer.builder()
+                        .name("Galaxy Cat")
+                        .style("PALE_ALE")
+                        .quantityToBrew(200)
+                        .minOnHand(12)
+                        .upc(337010000002L)
+                        .price(new BigDecimal("11.95"))
+                        .build());
+        log.info("... beer loader finished");
+    }
+}

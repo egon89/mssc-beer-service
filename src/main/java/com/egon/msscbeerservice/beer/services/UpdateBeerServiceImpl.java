f9ccf9acf93dd7e7f9d2583ca89631aa0e1b0d01
everton_goncalves <everton.goncalves.89@gmail.com>
2023-12-21 22:03:38 -0300

    feat: use repository in update service

--- src/main/java/com/egon/msscbeerservice/beer/services/UpdateBeerServiceImpl.java
+++ src/main/java/com/egon/msscbeerservice/beer/services/UpdateBeerServiceImpl.java
@@ -1,17 +1,37 @@
 package com.egon.msscbeerservice.beer.services;
 
 import com.egon.msscbeerservice.beer.dtos.BeerDto;
+import com.egon.msscbeerservice.beer.mappers.BeerMapper;
+import com.egon.msscbeerservice.beer.repositories.BeerRepository;
+import com.egon.msscbeerservice.shared.exceptions.NotFoundException;
+import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import lombok.val;
 import org.springframework.stereotype.Service;
 
 import java.util.UUID;
 
 @Slf4j
 @Service
+@RequiredArgsConstructor
 public class UpdateBeerServiceImpl implements UpdateBeerService {
+
+  private final BeerRepository repository;
+
+  private final BeerMapper mapper;
+
   @Override
   public BeerDto execute(UUID id, BeerDto beerDto) {
+    val beerEntity = repository.findById(id).orElseThrow(NotFoundException::new);
+    // TODO: refactor
+    beerEntity.setName(beerDto.getName());
+    beerEntity.setStyle(beerDto.getStyle().toString());
+    beerEntity.setPrice(beerDto.getPrice());
+    beerEntity.setUpc(beerDto.getUpc());
+
+    val beerUpdated = repository.save(beerEntity);
+
     log.debug("updating beer");
-    return null;
+    return mapper.toDto(beerUpdated);
   }
 }
---
+++ src/test/java/com/egon/msscbeerservice/beer/services/UpdateBeerServiceImplTest.java
@@ -0,0 +1,65 @@
+package com.egon.msscbeerservice.beer.services;
+
+import com.egon.msscbeerservice.beer.dtos.BeerDto;
+import com.egon.msscbeerservice.beer.enums.BeerStyleEnum;
+import com.egon.msscbeerservice.beer.helpers.BeerDtoHelper;
+import com.egon.msscbeerservice.beer.helpers.BeerEntityHelper;
+import com.egon.msscbeerservice.beer.helpers.BeerHelper;
+import com.egon.msscbeerservice.beer.repositories.BeerRepository;
+import com.egon.msscbeerservice.shared.exceptions.NotFoundException;
+import lombok.val;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+
+import java.math.BigDecimal;
+import java.util.Optional;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.*;
+
+@SpringBootTest
+class UpdateBeerServiceImplTest {
+
+  @Autowired
+  private UpdateBeerServiceImpl service;
+
+  @MockBean
+  private BeerRepository repository;
+
+  @Test
+  void shouldUpdateABeer() {
+    val beerEntity = BeerEntityHelper.create();
+    val updateBeerDto = BeerDto.builder()
+        .id(beerEntity.getId())
+        .name("Beer 2")
+        .style(BeerStyleEnum.IPA)
+        .price(BigDecimal.valueOf(11.5))
+        .upc(3L)
+        .build();
+    when(repository.findById(any())).thenReturn(Optional.of(beerEntity));
+    when(repository.save(any())).thenReturn(beerEntity);
+
+    val result = service.execute(BeerHelper.ID, updateBeerDto);
+
+    assertThat(result).isNotNull();
+    verify(repository).save(assertArg(p -> {
+      assertThat(p.getId()).isEqualTo(updateBeerDto.getId());
+      assertThat(p.getName()).isEqualTo(updateBeerDto.getName());
+      assertThat(p.getStyle()).isEqualTo(updateBeerDto.getStyle().toString());
+      assertThat(p.getPrice()).isEqualTo(updateBeerDto.getPrice());
+      assertThat(p.getUpc()).isEqualTo(updateBeerDto.getUpc());
+    }));
+  }
+
+  @Test
+  void ShouldThrowNotFoundExceptionWhenBeerDoesNotExist() {
+    when(repository.findById(any())).thenThrow(NotFoundException.class);
+
+    assertThrows(NotFoundException.class,
+        () -> service.execute(BeerHelper.ID, BeerDtoHelper.create()));
+  }
+}

c4c0d8ace58910f31f8c5d9dd45e2e7bc40d933f
everton_goncalves <everton.goncalves.89@gmail.com>
2024-01-11 14:31:03 -0300

    feat: add cache to find by id service

--- src/main/java/com/egon/msscbeerservice/beer/services/FindBeerByIdServiceImpl.java
+++ src/main/java/com/egon/msscbeerservice/beer/services/FindBeerByIdServiceImpl.java
@@ -5,10 +5,13 @@ import com.egon.msscbeerservice.beer.mappers.BeerMapper;
 import com.egon.msscbeerservice.beer.repositories.BeerRepository;
 import com.egon.msscbeerservice.shared.exceptions.NotFoundException;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.cache.annotation.Cacheable;
 import org.springframework.stereotype.Service;
 
 import java.util.UUID;
 
+@Slf4j
 @Service
 @RequiredArgsConstructor
 public class FindBeerByIdServiceImpl implements FindBeerByIdService {
@@ -17,8 +20,10 @@ public class FindBeerByIdServiceImpl implements FindBeerByIdService {
 
   private final BeerMapper mapper;
 
+  @Cacheable(cacheNames = "beerCache", key = "#id", condition = "#showInventoryOnHand == false ")
   @Override
   public BeerDto execute(UUID id, Boolean showInventoryOnHand) {
+    log.debug("called find beer by id for id %s".formatted(id.toString()));
     final var beerEntity = repository.findById(id).orElseThrow(NotFoundException::new);
 
     return mapper.toDto(beerEntity, showInventoryOnHand);

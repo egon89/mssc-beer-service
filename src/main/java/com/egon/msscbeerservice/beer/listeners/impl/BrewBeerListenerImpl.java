7eaaf9712496952763a4bd99bf99fb525278fa66
Everton Goncalves <everton.goncalves.89@gmail.com>
2024-02-13 13:17:31 -0300

    feat: brew beer listener

---
+++ src/main/java/com/egon/msscbeerservice/beer/listeners/impl/BrewBeerListenerImpl.java
@@ -0,0 +1,39 @@
+package com.egon.msscbeerservice.beer.listeners.impl;
+
+import com.egon.msscbeerservice.beer.dtos.events.BeerEventDto;
+import com.egon.msscbeerservice.beer.dtos.events.NewInventoryEventDto;
+import com.egon.msscbeerservice.beer.listeners.BrewBeerListener;
+import com.egon.msscbeerservice.beer.repositories.BeerRepository;
+import com.egon.msscbeerservice.config.JmsConfig;
+import com.egon.msscbeerservice.shared.exceptions.NotFoundException;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import lombok.val;
+import org.springframework.jms.annotation.JmsListener;
+import org.springframework.jms.core.JmsTemplate;
+import org.springframework.stereotype.Service;
+
+import java.util.Optional;
+
+@Slf4j
+@RequiredArgsConstructor
+@Service
+public class BrewBeerListenerImpl implements BrewBeerListener {
+
+  private final BeerRepository beerRepository;
+  private final JmsTemplate jmsTemplate;
+
+  @JmsListener(destination = JmsConfig.BREWING_REQUEST_QUEUE)
+  @Override
+  public void listen(BeerEventDto eventDto) {
+    val beerDto = Optional.of(eventDto.getData()).orElseThrow(IllegalArgumentException::new);
+    log.debug("Get beer {} listening queue {}", beerDto.id(), JmsConfig.BREWING_REQUEST_QUEUE);
+    val beer = beerRepository.findById(beerDto.id()).orElseThrow(NotFoundException::new);
+    val newInventoryBeerDto = new NewInventoryEventDto.NewInventoryBeerDto(beerDto.id(), beer.getQuantityToBrew());
+    val newInventoryBeerEventDto = NewInventoryEventDto.builder().data(newInventoryBeerDto).build();
+
+    jmsTemplate.convertAndSend(JmsConfig.NEW_INVENTORY_QUEUE, newInventoryBeerEventDto);
+    log.debug("Beer {} sent to queue {}", beerDto.id(), JmsConfig.NEW_INVENTORY_QUEUE);
+  }
+}
+
